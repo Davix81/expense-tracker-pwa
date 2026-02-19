91671cf2e3e28dc070aaf8fed0423e11859cd8fe
dterradellas <davidterradellas@gmail.com>
2026-02-19 22:28:38 +0100

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,7 +5,7 @@ export const environment = {
     password: '123'
   },
   github: {
-    token: '__GITHUB_TOKEN__', // Placeholder reemplazado en build time
+    token: 'EXPENSES_REPO_ACCESS_TOKEN', // Placeholder reemplazado en build time
     owner: 'Davix81',
     repo: 'expenses-data',
     branch: 'main',
